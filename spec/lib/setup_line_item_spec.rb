fbb2d72796855740cbf50fe8c777ae3305b08b34
William Holt <holtw@musc.edu>
2016-03-29 16:12:51 -0400

    added tests for setup_line_item class

--- spec/lib/setup_line_item_spec.rb
+++ spec/lib/setup_line_item_spec.rb
@@ -2,15 +2,48 @@ require 'rails_helper'
 
 RSpec.describe SetupLineItem do
   describe '#create_arm' do
-    it 'should create a new line item' do
-      protocol = build_stubbed(:protocol)
+    it 'should create a line item for the service' do
+      protocol = create(:protocol)
       service = create(:service)
       arm = create(:arm, protocol: protocol)
-      arm_creator = SetupLineItem.new(arm, [service])
+      setup_line_item = SetupLineItem.new(arm, [service])
 
-      service = arm_creator.create_line_item
+      service = setup_line_item.create_line_item
 
-      expect(line_item.protocol_id).to eq(arm.protocol_id)
+      expect(service.first.line_items.first.service_id).to eq(service.first.id)
+    end
+
+    it 'should have the arm_id of the new arm' do
+      protocol = create(:protocol)
+      service = create(:service)
+      arm = create(:arm, protocol: protocol)
+      setup_line_item = SetupLineItem.new(arm, [service])
+
+      service = setup_line_item.create_line_item
+
+      expect(service.first.line_items.first.arm_id).to eq(arm.id)
+    end
+
+    it 'should have the protocol id of the new arm' do
+      protocol = create(:protocol)
+      service = create(:service)
+      arm = create(:arm, protocol: protocol)
+      setup_line_item = SetupLineItem.new(arm, [service])
+
+      service = setup_line_item.create_line_item
+
+      expect(service.first.line_items.first.protocol_id).to eq(arm.protocol_id)
+    end
+
+    it 'should have the same subject count as the new arm' do
+      protocol = create(:protocol)
+      service = create(:service)
+      arm = create(:arm, protocol: protocol)
+      setup_line_item = SetupLineItem.new(arm, [service])
+
+      service = setup_line_item.create_line_item
+
+      expect(service.first.line_items.first.subject_count).to eq(arm.subject_count)
     end
   end
 end
